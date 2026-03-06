9aa8e46ea20e92aae90c5a808390184cdb30a064
StanislawMachnik <stanislaw.machnik@uczen.mechaniktg.pl>
2026-03-06 10:03:37 +0100

    Dodanie encji

---
+++ app/src/main/java/com/pt2/bazazadanie/Samochody.java
@@ -0,0 +1,9 @@
+package com.pt2.bazazadanie;
+
+public class Samochody {
+    private String model;
+    private String marka;
+    private int przebieg;
+    private double cena;
+    private String kraj;
+}
