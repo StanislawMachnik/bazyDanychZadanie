9d397d45ad778553386365e9973be04b53f1c4af
StanislawMachnik <stanislaw.machnik@uczen.mechaniktg.pl>
2026-03-06 10:18:04 +0100

    Dodanie Interfejsu - DAO, Utworzeni struktury bazy danych - Samochody.java

--- app/src/main/java/com/pt2/bazazadanie/Samochody.java
+++ app/src/main/java/com/pt2/bazazadanie/Samochody.java
@@ -1,9 +1,72 @@
 package com.pt2.bazazadanie;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity (tableName = "samochody_tabela")
 public class Samochody {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
     private String model;
     private String marka;
     private int przebieg;
     private double cena;
     private String kraj;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public int getPrzebieg() {
+        return przebieg;
+    }
+
+    public void setPrzebieg(int przebieg) {
+        this.przebieg = przebieg;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    public String getKraj() {
+        return kraj;
+    }
+
+    public void setKraj(String kraj) {
+        this.kraj = kraj;
+    }
+
+    public Samochody(String model, String marka, int przebieg, double cena, String kraj) {
+        this.id = 0;
+        this.model = model;
+        this.marka = marka;
+        this.przebieg = przebieg;
+        this.cena = cena;
+        this.kraj = kraj;
+    }
 }
---
+++ app/src/main/java/com/pt2/bazazadanie/SamochodyDao.java
@@ -0,0 +1,24 @@
+package com.pt2.bazazadanie;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface SamochodyDao {
+    @Insert
+    public void wstawSamochody(Samochody samochody);
+
+    @Delete
+    public void usun(Samochody samochody);
+
+    @Update
+    public void zaktualizuj(Samochody samochody);
+
+    @Query("Select * from samochody_tabela")
+    List<Samochody> zwrocWszystkieSamochody();
+}
